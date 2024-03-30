b8bb286c585447bd652440ade237df1ff3ebb00e
Cameron Dugan <cameron.dugan@protonmail.com>
2024-03-30 16:52:26 -0400

    improved sql

--- create_db.sql
+++ create_db.sql
@@ -1,17 +1,20 @@
 -- Create Student table
+-- DROP TABLE IF EXISTS Student;
 CREATE TABLE Student (
     StudentID INTEGER PRIMARY KEY,
     Name TEXT
 );
 
 -- Create Course table
-CREATE TABLE COURSE (
+-- DROP TABLE IF EXISTS Course;
+CREATE TABLE Course (
     CourseID INTEGER PRIMARY KEY,
     Name TEXT,
     Professor TEXT
 );
 
 -- Create Student Courses table with foreign key reference to Student table
+-- DROP TABLE IF EXISTS StudentCourses;
 CREATE TABLE StudentCourses (
     StudentID INTEGER,
     CourseID INTEGER,
@@ -21,6 +24,7 @@ CREATE TABLE StudentCourses (
 );
 
 -- Create Course Gradable table
+-- DROP TABLE IF EXISTS CourseGradeables;
 CREATE TABLE CourseGradeables (
     AssignmentID INTEGER PRIMARY KEY,
     CourseID INTEGER,
@@ -30,10 +34,11 @@ CREATE TABLE CourseGradeables (
 );
 
 -- Create Student Performance table with foreign key references to Student, Course Gradable tables
+-- DROP TABLE IF EXISTS StudentPerformance;
 CREATE TABLE StudentPerformance (
     StudentID INTEGER,
     AssignmentID INTEGER,
-    TimeManagement TEXT CHECK (TimeManagement IN ('too much', 'too little', 'just enough')),
+    TimeManagementFeedback TEXT CHECK (TimeManagementFeedback IN ('too much', 'too little', 'just enough')),
     Grade INTEGER,
     FOREIGN KEY (StudentID) REFERENCES Student(StudentID),
     FOREIGN KEY (AssignmentID) REFERENCES CourseGradeables(AssignmentID),
